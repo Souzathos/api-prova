04aa23eadb769b43de4fe50ef49097b3e10fd855
ATHOS FELIPE NASCIMENTO DE SOUZA <04564021010@senacrs.edu.br>
2026-04-18 11:45:50 -0300

    cors

--- src/middlewares/roleMiddlewares.ts
+++ src/middlewares/roleMiddlewares.ts
@@ -2,7 +2,7 @@ import {Request, Response, NextFunction} from 'express'
 
 export const roleMiddleware = (role:string) => {
     return (req:Request, res:Response, next:NextFunction) => {
-        if(req.user.role !== role) {
+        if((req as any).user.role !== role) {
             return res.status(403).json({ message: 'Acesso negado'})
         }
         next()
--- src/server.ts
+++ src/server.ts
@@ -2,10 +2,13 @@ import express from 'express'
 import { AppDataSource } from './config/dataSource'
 import routes from './routes'
 import * as dotenv from 'dotenv'
+import cors from 'cors'
+
 
 dotenv.config()
 const port = process.env.PORT
 const app = express()
+app.use(cors())
 
 app.use(express.json())
 
